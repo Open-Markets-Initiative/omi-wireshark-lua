64955ee4ff7a9f2470fe27fcc2d5ea5510835175
Open Markets Initiative <contact@openmarketsinitiative.com>
2026-09-16 14:14:47 -0500

    Add static value to Iex protocols

--- Iex/Iex_IexEquities_Tops_IexTp_v1_64_Dissector.lua
+++ Iex/Iex_IexEquities_Tops_IexTp_v1_64_Dissector.lua
@@ -703,7 +703,11 @@ iex_iexequities_tops_iextp_v1_64.message_protocol_id.size = 2
 
 -- Display: Message Protocol Id
 iex_iexequities_tops_iextp_v1_64.message_protocol_id.display = function(value)
-  return "Message Protocol Id: "..value
+  if value == 32771 then
+    return "Message Protocol Id: Iex Tops"
+  end
+
+  return "Message Protocol Id: Unknown("..value..")"
 end
 
 -- Dissect: Message Protocol Id
@@ -2467,7 +2471,7 @@ iex_iexequities_tops_iextp_v1_64.iextp_header.fields = function(buffer, offset, 
   -- Reserved: 1 Byte
   index, reserved = iex_iexequities_tops_iextp_v1_64.reserved.dissect(buffer, index, packet, parent)
 
-  -- Message Protocol Id: 2 Byte Unsigned Fixed Width Integer
+  -- Message Protocol Id: 2 Byte Unsigned Fixed Width Integer Static
   index, message_protocol_id = iex_iexequities_tops_iextp_v1_64.message_protocol_id.dissect(buffer, index, packet, parent)
 
   -- Channel Id: 4 Byte Unsigned Fixed Width Integer
@@ -2564,10 +2568,25 @@ end
 -- Protocol Heuristics
 -----------------------------------------------------------------------
 
+-- Verify Message Protocol Id Field
+iex_iexequities_tops_iextp_v1_64.message_protocol_id.verify = function(buffer)
+  -- Attempt to read field
+  local value = buffer(2, 2):le_uint()
+
+  if value == 32771 then
+    return true
+  end
+
+  return false
+end
+
 -- Dissector Heuristic for Iex IexEquities Tops IexTp 1.64 (Udp)
 local function omi_iex_iexequities_tops_iextp_v1_64_udp_heuristic(buffer, packet, parent)
   -- Verify packet length
   if not iex_iexequities_tops_iextp_v1_64.packet.requiredsize(buffer) then return false end
+
+  -- Verify Message Protocol Id
+  if not iex_iexequities_tops_iextp_v1_64.message_protocol_id.verify(buffer) then return false end
 
   -- Protocol is valid, set conversation and dissect this packet
   packet.conversation = omi_iex_iexequities_tops_iextp_v1_64
